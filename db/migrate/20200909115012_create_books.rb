a107f35896856d258ddcefcf1a5efbcb4c14bf2f
thiaguerd <mail@thiago.pro>
2020-09-09 06:50:24 -0500

    add model book

---
+++ db/migrate/20200909115012_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.references :author, null: false, foreign_key: true
+      t.string :isbn
+
+      t.timestamps
+    end
+  end
+end
